75aab931731315e59add2968ef9d93e8b1996d61
Imran <imran.bursagov@gmail.com>
2022-06-03 16:29:27 +0300

    imran

--- routes/products.route.js
+++ routes/products.route.js
@@ -1,7 +1,12 @@
 const { Router } = require("express");
 const router = Router();
+const {productsController} = require('../controllers/products.controller')
 
-router.post("/products", );
-
+router.get('/products', productsController.getProducts);
+router.get('/products/category/:id', productsController.getCategoryProducts);
+router.get('/products/brand/:id', productsController.getBrandProducts);
+router.post('admin/products', productsController.addProduct);
+router.patch('admin/products/:id', productsController.patchProduct);
+router.delete('admin/products/:id', productsController.deleteProduct);
 
 module.exports = router;
