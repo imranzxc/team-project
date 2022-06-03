3a5965934c84c82ce7f4ed52b4a92b9f7b7e32d0
Askhab <87871443+Askhab7777@users.noreply.github.com>
2022-06-03 15:57:57 +0300

    Сделал контроллеры категорий и роуты к ним

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,6 +1,56 @@
 const Category = require("../models/Category.model");
 
-
 module.exports.categoryController = {
+
+// Admin route
+  postCategory: async (req, res) => {
+    try {
+      const categoryPost = await Category.create({
+     title: String,
+      });
+      res.json(categoryPost);
+    } catch (error) {
+    res.json(error)
+    }
+  },
+
+  // Admin route
+  deleteCategory: async (req, res) => {
+      try {
+      const categoryDelete = await Category.findByIdAndRemove(req.params.id) 
+      res.json(categoryDelete)   
+      } catch (error) {
+       res.json(error)   
+      }
+  },
+
+// Admin route
+  patchCategory: async (req, res) => {
+      try {
+       const categoryPatch = await Category.findByIdAndUpdate({
+           title: String
+       })
+       res.json(categoryPatch)   
+      } catch (error) {
+         res.json(error) 
+      }
+  },
+
+
+
+  // User route
+AllCategory: async (req, res) => {
+    try {
+     const CategoryAll = await Category.find()   
+     res.json(CategoryAll)
+    } catch (error) {
+    res.json(error)  
+    }
+}
+
+
+
+
+
 
 };
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,7 +1,21 @@
 const { Router } = require("express");
+const { categoryController } = require("../controllers/category.controller");
 const router = Router();
 
-router.post("/category", );
+router.post("/admin/category", categoryController.postCategory);
+router.delete("/admin/category/:id", categoryController.deleteCategory)
+router.patch("/admin/category/:id", categoryController.patchCategory)
+router.get("/user/category", categoryController.AllCategory)
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
